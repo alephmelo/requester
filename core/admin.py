c5e7a5d8eb5332bd113e2568f66b44e96dfa0111
Aleph Melo <alephmelo@icloud.com>
2016-02-23 12:49:51 -0300

    Models, Admin Reg, Searchable fields.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Client, FeatureRequest
 
-# Register your models here.
+
+admin.site.unregister(Group)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'client', 'priority', 'target_date', 'url', 'product_area']
+    search_fields = ['title', 'client__name']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    """Client model"""
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+
+    def __str__(self):
+        return self.name
+
+
+class FeatureRequest(models.Model):
+    """Feature Request model"""
+    PRODUCT_AREA_CHOICES = (
+                            ('PL', 'Policies'),
+                            ('BL', 'Billing'),
+                            ('CL', 'Claims'),
+                            ('RP', 'Reports')
+    )
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    client = models.ForeignKey('Client')
+    priority = models.PositiveSmallIntegerField()
+    target_date = models.DateField()
+    url = models.URLField()
+    product_area = models.CharField(max_length=2, choices=PRODUCT_AREA_CHOICES)
+
+    class Meta:
+        verbose_name = "Feature Request"
+        verbose_name_plural = "Feature Requests"
+
+    def __str__(self):
+        return self.title
+    
