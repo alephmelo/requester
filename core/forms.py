7176333d3227fc1a579423ac48337ecd610c4a5f
Aleph Melo <alephmelo@icloud.com>
2016-02-23 15:38:42 -0300

    Index view, form view, bootstrap css.

---
+++ core/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import FeatureRequest
+
+class FeatureRequestForm(forms.ModelForm):
+    """Feature Request form"""
+    class Meta:
+            model = FeatureRequest
+            fields = ['title','description','client','priority','target_date','url','product_area']
+            widgets = {
+               'title': forms.TextInput(attrs={
+                    'class': 'form-control',
+                    'placeholder':'Title'
+                }),
+               'description': forms.TextInput(attrs={
+                    'class': 'form-control',
+                    'placeholder':'Description'
+                }),
+               'client': forms.Select(attrs={
+                    'class': 'form-control'
+                }),
+               'priority': forms.TextInput(attrs={
+                    'class': 'form-control',
+                    'placeholder':'Priority'
+                }),
+               'target_date': forms.DateInput(attrs={
+                    'class': 'form-control',
+                    'id': 'flatpickr',
+                    'placeholder':'YYYY-MM-DD'
+                }),
+               'url': forms.TextInput(attrs={
+                    'class': 'form-control',
+                    'placeholder':'URL'
+                }),
+               'product_area': forms.Select(attrs={
+                    'class': 'form-control'
+                })
+            }
--- core/views.py
+++ core/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-import datetime
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import FeatureRequest
+from .forms import FeatureRequestForm
+
 
 def index(request):
-    return render(request, 'index.html')
+    results = FeatureRequest.objects.all()
+    return render(request, 'index.html', {'results':results})
+
+
+def new_request(request):
+    if request.method == "POST":
+        form = FeatureRequestForm(request.POST)
+        if form.is_valid():
+            new_feature_request = form.save(commit=False)
+            new_feature_request.save()
+            return HttpResponseRedirect(reverse('core:index'))
+    else:
+        form = FeatureRequestForm()
+
+    return render(request, 'new_request.html', {'form': form})
