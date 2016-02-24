8f860d64360bd4336f7920d6eedad939472cbf03
Aleph Melo <alephmelo@icloud.com>
2016-02-24 15:19:14 -0300

    Apply pep8.

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from .forms import FeatureRequestForm
 
 def index(request):
     results = FeatureRequest.objects.all().order_by('priority', 'client_id')
-    return render(request, 'index.html', {'results':results})
+    return render(request, 'index.html', {'results': results})
 
 
 def new_request(request):
@@ -20,10 +20,11 @@ def new_request(request):
                 client_id=new_feature_request.client_id).order_by('priority')
 
             if queryset.filter(priority=new_feature_request.priority).exists():
-                dict_index_priority = {index: item.priority for index, item in enumerate(queryset)}
-                
+                dict_index_priority = {
+                    index: item.priority for index, item in enumerate(queryset)}
+
                 index = [key for key, value in dict_index_priority.items() \
-                        if value == new_feature_request.priority][0]
+                         if value == new_feature_request.priority][0]
 
                 for result in queryset[index:]:
                     result.priority += 1
